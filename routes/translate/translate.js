7979ca8fede6b2c21942e132807a35134eba60d5
Joseph Perthold <Bedlesssgod@gmail.com>
2024-10-14 00:03:47 +0200

    Added More scrubbers, added Body Injection

--- routes/translate/translate.js
+++ routes/translate/translate.js
@@ -1,8 +1,15 @@
 import load from './loader.js';
+import fs from 'fs';
 
+const translationkey = JSON.parse(fs.readFileSync('./routes/translate/transkey.json', 'utf8')); 
 
 const translationMiddleWare = async (content) => {
-    content = await scrub(content, [h2Scrubber, h1Scrubber]);
+    content = await scrub(content, [
+        handleLineBr1, handleLineBr2,
+        handleNormalText,
+        handleBold,
+        h5Scrubber, h4Scrubber, h3Scrubber, h2Scrubber, h1Scrubber,
+        headInject]);
     console.log("Finished Translation Middleware");
     return content;
 }
@@ -18,12 +25,63 @@ const scrub = async(content, scrubbers) => {
     return content;
 }
 
+const headInject = async(content) => {
+    let keyed = translationkey['MASTER'];
+    return `<body style='${keyed.style}'>` + content + `</body>`;
+}
+
 const h1Scrubber = async(content) => {
-    return content.replaceAll('#', '<h1>');
+    let keyed = translationkey['#'];
+    return content.replaceAll('#', keyed.oTag.replace('%style%', keyed.style));
 }
 
 const h2Scrubber = async(content) => {
-    return content.replaceAll('##', '<h2>');
+    let keyed = translationkey['##'];
+    return content.replaceAll('##', keyed.oTag.replace('%style%', keyed.style));
+}
+
+const h3Scrubber = async(content) => {
+    let keyed = translationkey['###'];
+    return content.replaceAll('###', keyed.oTag.replace('%style%', keyed.style));
+}
+
+const h4Scrubber = async(content) => {
+    let keyed = translationkey['####'];
+    return content.replaceAll('####', keyed.oTag.replace('%style%', keyed.style));
+}
+
+const h5Scrubber = async(content) => {
+    let keyed = translationkey['#####'];
+    return content.replaceAll('#####', keyed.oTag.replace('%style%', keyed.style));
+}
+
+const handleLineBr1 = async(content) => {
+    if(content.includes('---')) {
+        content = content.replaceAll('---', '<hr>');
+    }
+    return content;
+}
+
+const handleLineBr2 = async(content) => {
+    if(content.includes('***')) {
+        content = content.replaceAll('***', '<hr>');
+    }
+    return content;
+}
+
+const handleNormalText = async(content) => {
+    const lines = content.split('\n');
+    content = lines.map(line => {
+        if (!line.startsWith('#')) {
+            return `<p>${line}</p>`;
+        }
+        return line;
+    }).join('\n');
+    return content;
+}
+
+const handleBold = async(content) => {
+    return content.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
 }
 
 const handle = async(app) => {
